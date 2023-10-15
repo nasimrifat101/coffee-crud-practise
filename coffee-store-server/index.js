8a47dd171b7d9670622daaf0caf46872aa9f8a30
nasimrifat101 <nasimrifat101@gmail.com>
2023-10-15 23:15:16 +0600

    minor issue fix

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMaster
-// bOqpbQYY7mSM2vLI
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuxyuxp.mongodb.net/?retryWrites=true&w=majority`;
 
